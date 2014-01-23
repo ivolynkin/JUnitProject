8fbc88bdaa5ee30d6847835c32a551694735212e
Ivan Volynkin <ivan_volynkin@student.uml.edu>
2014-01-23 18:40:10 -0500

    Created assert tests for Vector

--- JUnit-Sample/test/sample/VectorsJUnitTest.java
+++ JUnit-Sample/test/sample/VectorsJUnitTest.java
@@ -30,29 +30,39 @@ public class VectorsJUnitTest {
      * Test of equal method, of class Vectors.
      */
     @Test
-    public void testEqual() {
-        System.out.println("equal");
-        int[] a = null;
-        int[] b = null;
-        boolean expResult = false;
-        boolean result = Vectors.equal(a, b);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void equalsCheck() {
+    System.out.println("* VectorsJUnit4Test: equalsCheck()");
+    assertTrue(Vectors.equal(new int[] {}, new int[] {}));
+    assertTrue(Vectors.equal(new int[] {0}, new int[] {0}));
+    assertTrue(Vectors.equal(new int[] {0, 0}, new int[] {0, 0}));
+    assertTrue(Vectors.equal(new int[] {0, 0, 0}, new int[] {0, 0, 0}));
+    assertTrue(Vectors.equal(new int[] {5, 6, 7}, new int[] {5, 6, 7}));
+
+    assertFalse(Vectors.equal(new int[] {}, new int[] {0}));
+    assertFalse(Vectors.equal(new int[] {0}, new int[] {0, 0}));
+    assertFalse(Vectors.equal(new int[] {0, 0}, new int[] {0, 0, 0}));
+    assertFalse(Vectors.equal(new int[] {0, 0, 0}, new int[] {0, 0}));
+    assertFalse(Vectors.equal(new int[] {0, 0}, new int[] {0}));
+    assertFalse(Vectors.equal(new int[] {0}, new int[] {}));
+
+    assertFalse(Vectors.equal(new int[] {0, 0, 0}, new int[] {0, 0, 1}));
+    assertFalse(Vectors.equal(new int[] {0, 0, 0}, new int[] {0, 1, 0}));
+    assertFalse(Vectors.equal(new int[] {0, 0, 0}, new int[] {1, 0, 0}));
+    assertFalse(Vectors.equal(new int[] {0, 0, 1}, new int[] {0, 0, 3}));
+        
     }
 
     /**
      * Test of scalarMultiplication method, of class Vectors.
      */
     @Test
-    public void testScalarMultiplication() {
-        System.out.println("scalarMultiplication");
-        int[] a = null;
-        int[] b = null;
-        int expResult = 0;
-        int result = Vectors.scalarMultiplication(a, b);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void ScalarMultiplication() {
+    System.out.println("* VectorsJUnit4Test: ScalarMultiplicationCheck()");
+    assertEquals(  0, Vectors.scalarMultiplication(new int[] { 0, 0}, new int[] { 0, 0}));
+    assertEquals( 39, Vectors.scalarMultiplication(new int[] { 3, 4}, new int[] { 5, 6}));
+    assertEquals(-39, Vectors.scalarMultiplication(new int[] {-3, 4}, new int[] { 5,-6}));
+    assertEquals(  0, Vectors.scalarMultiplication(new int[] { 5, 9}, new int[] {-9, 5}));
+    assertEquals(100, Vectors.scalarMultiplication(new int[] { 6, 8}, new int[] { 6, 8}));
+        
     }
 }
