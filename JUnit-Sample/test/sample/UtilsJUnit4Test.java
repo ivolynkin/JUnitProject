6dfc4b59c670cf06a0ec565e9a7b2d2e57cd4eea
Ivan Volynkin <ivan_volynkin@student.uml.edu>
2014-01-23 19:19:23 -0500

    Completed UtilsJUnit4Test.java

--- JUnit-Sample/test/sample/UtilsJUnit4Test.java
+++ JUnit-Sample/test/sample/UtilsJUnit4Test.java
@@ -9,6 +9,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Ignore;
 import static org.junit.Assert.*;
 
 /**
@@ -22,59 +23,60 @@ public class UtilsJUnit4Test {
     
     @BeforeClass
     public static void setUpClass() {
+         System.out.println("* UtilsJUnit4Test: @BeforeClass method");
     }
     
     @AfterClass
     public static void tearDownClass() {
+        System.out.println("* UtilsJUnit4Test: @AfterClass method");
     }
     
     @Before
     public void setUp() {
+        System.out.println("* UtilsJUnit4Test: @Before method");
     }
     
     @After
     public void tearDown() {
+        System.out.println("* UtilsJUnit4Test: @After method");
     }
 
     /**
      * Test of concatWords method, of class Utils.
      */
     @Test
-    public void testConcatWords() {
-        System.out.println("concatWords");
-        String[] words = null;
-        String expResult = "";
-        String result = Utils.concatWords(words);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void helloWorldCheck() {
+        System.out.println("* UtilsJUnit4Test: test method 1 - helloWorldCheck()");
+        assertEquals("Hello, world!", Utils.concatWords("Hello", ", ", "world", "!"));
     }
 
     /**
      * Test of computeFactorial method, of class Utils.
      */
-    @Test
-    public void testComputeFactorial() {
-        System.out.println("computeFactorial");
-        int number = 0;
-        String expResult = "";
-        String result = Utils.computeFactorial(number);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @Test(timeout=1000)
+    public void testWithTimeout() {
+         System.out.println("* UtilsJUnit4Test: test method 2 - testWithTimeout()");
+         final int factorialOf = 1 + (int) (30000 * Math.random());
+         System.out.println("computing " + factorialOf + '!');
+         System.out.println(factorialOf + "! = " + Utils.computeFactorial(factorialOf));
     }
-
-    /**
+    
+    @Test (expected=IllegalArgumentException.class)
+    public void checkExpectedException() {
+         System.out.println("* UtilsJUnit4Test: test method 3 - checkExpectedException()");
+         final int factorialOf = -5;
+         System.out.println(factorialOf + "! = " + Utils.computeFactorial(factorialOf));
+    }
+    
+    
+      /**
      * Test of normalizeWord method, of class Utils.
      */
+    @Ignore
     @Test
-    public void testNormalizeWord() {
-        System.out.println("normalizeWord");
-        String word = "";
-        String expResult = "";
-        String result = Utils.normalizeWord(word);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void temporarilyDisabledTest() throws Exception {
+     System.out.println("* UtilsJUnit4Test: test method 4 - checkExpectedException()");
+     assertEquals("Malm\u00f6", Utils.normalizeWord("Malmo\u0308"));
     }
+    
 }
