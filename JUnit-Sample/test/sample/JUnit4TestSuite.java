612aa3aa9148a8933185423483b9a2c8f9a9c0f5
Ivan Volynkin <ivan_volynkin@student.uml.edu>
2014-01-23 19:50:06 -0500

    Final commit Test Suit is created

---
+++ JUnit-Sample/test/sample/JUnit4TestSuite.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Ivan
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sample.VectorsJUnitTest.class, sample.UtilsJUnit4Test.class})
+public class JUnit4TestSuite {
+    
+}
